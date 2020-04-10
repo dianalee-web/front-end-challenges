90707dfd003056f8e88d44fcc3c6afacdfb539f2
Diana Telahun <telahun.diana@gmail.com>
2020-04-09 22:32:07 -0700

    html,css , js added. Need to validate code

--- lesson-1/js/main.js
+++ lesson-1/js/main.js
@@ -1,9 +1,16 @@
-
 /*
-* JAVASCRIPT FUNCTION
-* @parameter: This does ____
-*
-* Notes: Lorem ipsum justo vel
-* */
+ * JAVASCRIPT FUNCTION
+ * @parameter: This does ____
+ *
+ * Notes: Lorem ipsum justo vel
+ * */
 
-console.log("This outputs into the console.");
+//console.log("This outputs into the console.");
+
+let funFactButton = document.getElementById('funFactButton');
+
+funFactButton.addEventListener('click', function () {
+  alert(
+    'FUN FACT: My kids and I take Jiu-Jitsu together and my 8 year old son is almost....just ALMOST strong enough to get out of my side control! OSS!'
+  );
+});
